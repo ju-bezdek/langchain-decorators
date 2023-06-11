6f0de8387f52f6d7eb0d0c5ff857629846cc0dc8
ju-bezdek <juraj.bezdek@gmail.com>
2023-06-11 14:36:21 +0200

    inc version

--- src/langchain_decorators/__init__.py
+++ src/langchain_decorators/__init__.py
@@ -2,4 +2,4 @@ from .common import LogColors, GlobalSettings, print_log, PromptTypes, PromptTyp
 from .prompt_decorator import PromptDecoratorTemplate
 from .streaming_context import StreamingContext
 from .prompt_decorator import llm_prompt
-__version__="0.0.2"
+__version__="0.0.3"
