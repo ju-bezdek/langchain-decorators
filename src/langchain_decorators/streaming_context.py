8b7702b75a9df2e1d11388021d763b6e189ef176
ju-bezdek <juraj.bezdek@gmail.com>
2023-06-11 13:16:34 +0200

    Merge branch 'lukestanley_llm_load_fix_and_linting'

--- src/langchain_decorators/streaming_context.py
+++ src/langchain_decorators/streaming_context.py
@@ -1,31 +1,26 @@
-
-
-
 import contextvars
 
 from typing import Any, Callable
 
 
-
 class StreamingContext():
     from langchain.callbacks.base import AsyncCallbackHandler
+
     class StreamingContextCallback(AsyncCallbackHandler):
 
         async def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
             StreamingContext.get_context().on_new_token(token)
 
-        async def on_llm_end(self, response , *args, **kwargs):
+        async def on_llm_end(self, response, *args, **kwargs):
             if StreamingContext.get_context().stream_to_stdout:
                 print()
-                
 
     context_var = contextvars.ContextVar('streaming_context')
 
-    def __init__(self, callback:Callable[[str],None]=None, stream_to_stdout:bool=False) -> None:
+    def __init__(self, callback: Callable[[str], None] = None, stream_to_stdout: bool = False) -> None:
         self.callback = callback
         self.stream_to_stdout = stream_to_stdout
-        self.token_colors = ['\033[90m', '\033[0m'] 
-        
+        self.token_colors = ['\033[90m', '\033[0m']
 
     def __enter__(self):
         self.__class__.context_var.set(self)
@@ -33,17 +28,16 @@ class StreamingContext():
     @classmethod
     def get_context(cls) -> 'StreamingContext':
         return cls.context_var.get("streaming_context")
-    
-    def on_new_token(self, token:str):
+
+    def on_new_token(self, token: str):
         if self.callback:
             self.callback(token)
         if self.stream_to_stdout:
             reset_color = '\033[0m'
             current_color = self.token_colors[0]
             self.token_colors.reverse()
-            print('{}{}{}'.format(current_color, token if token != "" else '\u2022', reset_color), end='')
-            
+            print('{}{}{}'.format(current_color, token if token !=
+                  "" else '\u2022', reset_color), end='')
 
-    
     def __exit__(self, exc_type, exc_value, traceback):
         self.context_var.set(None)
