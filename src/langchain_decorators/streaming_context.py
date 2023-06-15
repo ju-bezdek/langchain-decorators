4d20262404d9a0bdd9a195b261a670c9a6e914fe
ju-bezdek <juraj.bezdek@gmail.com>
2023-06-15 15:31:24 +0200

    improved streaming colors

--- src/langchain_decorators/streaming_context.py
+++ src/langchain_decorators/streaming_context.py
@@ -30,14 +30,18 @@ class StreamingContext():
         return cls.context_var.get(None)
 
     def on_new_token(self, token: str):
-        if self.callback:
-            self.callback(token)
-        if self.stream_to_stdout:
-            reset_color = '\033[0m'
-            current_color = self.token_colors[0]
-            self.token_colors.reverse()
-            print('{}{}{}'.format(current_color, token if token !=
-                  "" else '\u2022', reset_color), end='')
+        if token:
+            if self.callback:
+                self.callback(token)
+            if self.stream_to_stdout:
+                reset_color = '\033[0m'
+                if token.strip() and token!="\n":
+                    current_color = self.token_colors[0]
+                    self.token_colors.reverse()
+                else:
+                    current_color='\033[40m'
+                print('{}{}{}'.format(current_color, token if token !=
+                    "" else '\u2022', reset_color), end='')
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.context_var.set(None)
